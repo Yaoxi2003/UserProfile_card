67daec51337cc841cc296b0725b9b7cb8e1cde02
Yaoxi2003 <Y3517199512@gmail.com>
2025-10-02 11:02:35 +1000

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 
 export default function Card(props) {
     const [shareLink, setShareLink] = useState(false)
-    const menu = (<div className='absolute bg-slate-700 top-139 bottom-0 left-0 right-0 flex items-center gap-6 py-4 px-7 rounded-b-xl z-[2] md:rounded-xl md:top-40 md:bottom-25 md:left-150 md:-right-15'>
+    const menu = (<div className='absolute bg-slate-700 top-139 bottom-0 left-0 right-0 flex items-center gap-6 py-4 px-7 rounded-b-xl z-[2] md:rounded-xl md:top-40 md:bottom-25 md:left-135 md:-right-15'>
                     <h2 className='tracking-[5px] uppercase text-slate-400'>share</h2>
                     <a href="https://www.facebook.com/"><img src={props.facebookIcon} alt="facebook icon" className='w-5 h-5'/></a>
                     <a href="https://https://x.com/i/flow/login?redirect_after_login=%2Fexplore.pinterest.com/"><img src={props.twitterIcon} alt="twitter icon" className='w-5 h-5'/></a>
                     <a href="https://nl.pinterest.com/"><img src={props.pinterestIcon} alt="pinterest icon" className='w-5 h-5'/></a>
+                    <button onClick={handleEvent} className='rounded-full bg-slate-400 w-9 h-9 flex justify-center items-center ml-auto md:hidden'><img src={props.shareIcon} className='w-6 h-6'/></button>
                 </div>)
 
     function handleEvent() {
@@ -14,8 +15,8 @@ export default function Card(props) {
     }
 
     return (
-        <article className='relative flex flex-col rounded-xl shadow bg-white w-4/5 min-w-[320px] max-w-[340px] md:flex-row md:justify-between md:items-center md:max-w-[800px]'>
-            <img src={props.img} className='object-cover w-full rounded-t-xl lg-img md:w-1/3 md:rounded-l-xl md:h-full'/>
+        <article className='relative flex flex-col rounded-xl shadow bg-white w-4/5 min-w-[320px] max-w-[340px] md:flex-row md:justify-between md:max-w-[740px]'>
+            <img src={props.img} className='block object-cover w-full rounded-t-xl lg-img md:w-2/5 md:rounded-l-xl md:rounded-tr-none md:self-stretch'/>
             <div className='article-content p-8'>
                 <h2 className='text-xl text-slate-600 font-bold'>{props.articleTitle}</h2>
                 <p className='text-sm my-6 leading-[1.7]'>{props.articleContent}</p>
@@ -25,11 +26,12 @@ export default function Card(props) {
                         <h3 className='text-sm text-slate-600'>{props.profile.name}</h3>
                         <p className='text-xs'>{props.profile.postTime}</p>
                     </div>
-                    <button onClick={handleEvent} className='rounded-full bg-slate-400 w-9 h-9 flex justify-center items-center ml-auto z-3'><img src={props.shareIcon} className='w-6 h-6'/></button>
+                    <button onClick={handleEvent} className='rounded-full bg-slate-400 w-9 h-9 flex justify-center items-center ml-auto'><img src={props.shareIcon} className='w-6 h-6'/></button>
                 </div>
             </div>
             {shareLink && <div id="share-menu">{menu}</div>}
         </article>
     )
 }
+
 
